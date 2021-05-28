f26440172bf26af75a3326aee60b8ca3e9175b8c
umesh <umeshpurohit1975@gamil.com>
2021-05-28 17:34:09 +0530

    improved the emails

--- TaskApp/settings.py
+++ TaskApp/settings.py
@@ -143,11 +143,11 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp-mail.outlook.com'
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'umeshpurohit1975@outlook.com'
-EMAIL_HOST_PASSWORD = 'shri2007'
+EMAIL_HOST_USER = 'shriyanshpurohit2007@gmail.com'
+EMAIL_HOST_PASSWORD = 'pant1975varsity'
 
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'index'
