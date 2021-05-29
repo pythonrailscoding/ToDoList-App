454b439a00c74d0c66e2e7770f626884d518b920
umesh <umeshpurohit1975@gamil.com>
2021-05-29 19:38:09 +0530

    init

--- Feedback/urls.py
+++ Feedback/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
 	path('list_feed/', views.Create, name='create-index'),
 	path('edit_feed/<int:pk>/', views.edit, name='f_edit'),
 	path('delete_feed/<int:pk>/', views.delete, name='delete-feed'),
-	path("user_feedback_on_delete?why/feedback_on_why_delete/token?/nfjkqerjfjeqrjCBJBCRJBKRJECBJbvjkebvjbrtjknvbkjtrnhkjnjbvtkjrbvjktrnbtrjbvkrbhkjewrvbtjkbvjr", views.feedback_on_delete, name="feed-long"),
+	path("user_feedback_on_delete/", views.feedback_on_delete, name="feed-long"),
 ]
 
