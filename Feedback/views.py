7a32e2292ca8fc571752ac749af1efdc91eee667
umesh <umeshpurohit1975@gamil.com>
2021-05-29 10:29:19 +0530

    feedback email

--- Feedback/views.py
+++ Feedback/views.py
@@ -1,14 +1,27 @@
+from django import template
 from django.shortcuts import render, redirect
 from members.models import FeedBack
 from .forms import CreateFeedBack
 from django.contrib.auth.decorators import login_required
+from django.core.mail import EmailMessage
+from django.conf import settings
+from django.template.loader import render_to_string
 
 def Create(request):
 	list_feed = FeedBack.objects.all().order_by('-id')
 	form = CreateFeedBack(request.POST or None)
+	template = render_to_string("email.html", {"user": request.user.username})
 	if request.method == 'POST':
 		if form.is_valid():
 			form.save()
+			email = EmailMessage(
+				"Thanks for your feedback!",
+				template,
+				settings.EMAIL_HOST_USER,
+				[request.user.email,],
+			)
+			email.fail_silently = False
+			email.send()
 		return redirect('create-index')
 	# ordering = ['-id']
 	return render(request, 'list.html', {'list_feed': list_feed, 'form': form})
