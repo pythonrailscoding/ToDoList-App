ee9f3d1661ae2d927498135bb05bcdd7cc544213
umesh <umeshpurohit1975@gamil.com>
2021-05-29 19:53:16 +0530

    init

--- Feedback/forms.py
+++ Feedback/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from members.models import FeedBack
+from .models import FeedBackOnDelete
 
 class CreateFeedBack(forms.ModelForm):
 	class Meta:
@@ -14,3 +15,18 @@ class CreateFeedBack(forms.ModelForm):
 		labels = {
 			'body': ''
 		}
+
+class FeedBackModelForm(forms.ModelForm):
+	class Meta:
+		model = FeedBackOnDelete
+		fields = '__all__'
+
+		widgets = {
+			'feed': forms.Textarea(attrs={"class": "form-control", "rows": 4, "cols":20, "placeholder": "Enter Feedback"}),
+		}
+
+		labels = {
+			"feed": "",
+		}
+
+
