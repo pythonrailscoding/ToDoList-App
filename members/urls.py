f970ce96155ceccd013c6c556aaae69ebb48e67a
umesh <umeshpurohit1975@gmail.com>
2021-06-04 16:43:06 +0530

    added password reset

--- members/urls.py
+++ members/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import ChangeUser, ChangePassword, Register
 from . import views
+from django.contrib.auth.views import PasswordResetDoneView, PasswordResetView, PasswordResetConfirmView, PasswordResetCompleteView 
 
 urlpatterns = [
 	# path('register', RegisterUser.as_view(), name='register'),
@@ -9,4 +10,9 @@ urlpatterns = [
 	path('change_user_credentials/password/<int:pk>/', ChangePassword.as_view(), name='password'),
 	path('delete_user/<int:pk>/', views.delete_user, name='del'),
 	path("confirm_user_delete/", views.confirm_delete, name="con"),
+	path('password_reset/',PasswordResetView.as_view(template_name="accounts/password_reset.html"),name='password_reset'),
+	path('password_reset_done/', PasswordResetDoneView.as_view(template_name="accounts/password_reset_done.html"),name='password_reset_done'),
+	path('reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(template_name="accounts/password_reset_confirm.html"),name='password_reset_confirm'),
+	path('reset/done/', PasswordResetCompleteView.as_view(template_name="accounts/password_reset_complete.html"),name='password_reset_complete'),
+	
 ]
